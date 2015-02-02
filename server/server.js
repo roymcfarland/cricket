f52137804ebd395724798b3ad256d7a798f9d183
Brooks Patton <brooks.patton@latitude40.com>
2015-02-02 13:38:30 -0700

    Super basic server set up which allows the angular side to be public to the world.

--- server/server.js
+++ server/server.js
@@ -1,3 +1,9 @@
 var express = require('express');
 
 var app = express();
+
+app.use(express.static(__dirname + '/../app'));
+
+var server = app.listen(3000, function() {
+	console.log('Server running on port 3000');
+});
