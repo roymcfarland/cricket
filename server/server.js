cddd9d711d332a89aa410cb7e2d1dbe182b9e296
Brooks Patton <brooks.patton@latitude40.com>
2015-02-03 14:28:25 -0700

    Heroku setup
    Setting up server to work with Heroku.

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ var app = express();
 app.use(express.static(__dirname + '/../app'));
 app.use(webRoutes);
 
-var server = app.listen(3000, function() {
+var port = process.env.PORT || 3000;
+var server = app.listen(port, function() {
 	console.log('Server running on port 3000');
 });
