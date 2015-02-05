c6ef6dcfd62049db34b03c6e070ab5fa581624f6
Roy McFarland <roy.mcfarland@gmail.com>
2015-02-05 08:58:13 -0700

    Added routerRoutes and userService factory

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -1,30 +1,49 @@
-angular.module("routerApp", [])
-
-// Controllers
+angular.module("fantasyCricket", ["routerRoutes", "userService"])
 
 // mainController for the ENTIRE site
 .controller("mainController", function() {
-
 	var vm = this;
-
 	vm.bigMessage = "Hello World!";
-
 });
 
 // homeController for index.html
 .controller("homeController", function() {
-
 	var vm = this;
-
 	vm.message = "This is the home page!";
+});
 
+// leaderboardController for leaderboard.html
+controller("leaderboardController", function() {
+	var vm = this;
+	vm.message = "This is the leaderboard...";
+});
+
+// tradesController for trades.html
+controller("tradesController", function() {
+	var vm = this;
+	vm.message = "These are the trades...";
+});
+
+// myScoreController for myscore.html
+controller("myScoreController", function(){
+	var vm = this;
+	vm.message = "These are the user's points...";
 });
 
 // rulesController for rules.html
 .controller("rulesController", function() {
-
 	var vm = this;
+	vm.message = "These are the rules!";
+});
 
-	vm.message = "This is the rules page!"
+// legalController for legal.html
+.controller("legalTermsController", function() {
+	var vm = this;
+	vm.message = "This is our legal text.";
+});
 
+// contactController for contact info
+.controller("contactInfoController", function() {
+	var vm = this;
+	vm.message = "This is our contact info...";
 });
---
+++ app/public/js/app.routes.js
@@ -0,0 +1,58 @@
+angular.module("routerRoutes", ["ngRoute"])
+
+.config(function($routeProvider, $locationProvider) {
+	$routeProvider
+
+		// route for the home page
+		.when("/", {
+			templateUrl: "../views/pages/home.html",
+			controller: "homeController",
+			controllerAs: "home"
+		})
+
+		// route for the leaderboard
+		.when("/leaderboard", {
+			templateUrl: "../views/pages/leaderboard.html",
+			controller: "leaderboardController",
+			controllerAs: "leaderboard"
+		})
+
+		// route for trades
+		.when("/trades", {
+			templateUrl: "../views/pages/trades.html",
+			controller: "tradesController",
+			controllerAs: "trades"
+		})
+
+		// route for my score
+		.when("/myscore", {
+			templateUrl: "../views/pages/myscore.html",
+			controller: "myScoreController",
+			controllerAs: "myScore"
+		})
+
+		// route for rules
+		.when("/rules", {
+			templateUrl: "../views/pages/rules.html",
+			controller: "rulesController",
+			controllerAs: "rules"
+		})
+
+		// route for legal terms
+		.when("/legal", {
+			templateUrl: "../views/pages/legal.html",
+			controller: "legalTermsController",
+			controllerAs: "legalTerms"
+		})
+
+		// route for contact page
+		.when("/contact", {
+			templateUrl: "../views/pages/contact.html",
+			controller: "contactInfoController",
+			controllerAs: "contactInfo"
+		})
+
+	// Set app to use "pretty" URLs with no #
+	$locationProvider.html5Mode(true);
+
+});
