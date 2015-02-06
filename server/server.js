86ddafa984b7b38d6bf0adce70e659e88b70a8a4
Brooks Patton <brooks.patton@latitude40.com>
2015-02-05 17:18:03 -0700

    Got angular css files loaded up.

--- server/server.js
+++ server/server.js
@@ -3,8 +3,12 @@ var webRoutes = require('./web-routes');
 
 var app = express();
 
-app.use(express.static(__dirname + '/../app'));
-app.use(webRoutes);
+console.log('************');
+console.log(__dirname);
+console.log('************');
+
+app.use(express.static(__dirname + '/../app/public'));
+// app.use(webRoutes);
 
 var port = process.env.PORT || 3000;
 var server = app.listen(port, function() {
