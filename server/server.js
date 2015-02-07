13dcbeee5736260ef7fe3292c288312d2e409d54
Brooks Patton <brooks.patton@latitude40.com>
2015-02-07 13:51:11 -0700

    The server is no serving up the rules on the /route url. The test for getting the rules correctly have been put in place.

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var webRoutes = require('./web-routes');
+var dataRoutes = require('./data-routes');
 
 var app = express();
 
 app.use(express.static(__dirname + '/../app'));
+app.use(dataRoutes);
 app.use(webRoutes);
 
 var port = process.env.PORT || 3000;
--- server/test/server-test.js
+++ server/test/server-test.js
@@ -28,3 +28,40 @@ describe('the rules', function() {
 		});
 	});
 });
+
+describe('sending a GET to /rules', function() {
+	it('should get the rules in JSON format', function(done) {
+		request
+			.get('/rules')
+			.expect(200)
+			.end(function(err, res) {
+				if(err) return done(err);
+
+				res.body.captainChanges.should.be.exactly('unlimited');
+				res.body.moneyToCreateTeam.should.be.exactly(100000);
+				res.body.numberOfPlayersPerTeam.should.be.exactly(11);
+				res.body.points.basedOn.should.have.length(4);
+				res.body.points.bonus.BowlingEconomyRate['0.00-4.00'].should.be.exactly(15);
+				res.body.points.bonus.BowlingEconomyRate['4.01-5.50'].should.be.exactly(10);
+				res.body.points.bonus.BowlingEconomyRate['5.51-7.00'].should.be.exactly(0);
+				res.body.points.bonus.BowlingEconomyRate['7.01-9.00'].should.be.exactly(-10);
+				res.body.points.bonus.BowlingEconomyRate['9.01+'].should.be.exactly(-15);
+				res.body.points.bonus.every50Runs.should.be.exactly(20);
+				res.body.points.bonus.manOfTheMatch.should.be.exactly(20);
+				res.body.points.coreScoring.catch.should.be.exactly(10);
+				res.body.points.coreScoring.run.should.be.exactly(1);
+				res.body.points.coreScoring.runOut.should.be.exactly(10);
+				res.body.points.coreScoring.stump.should.be.exactly(10);
+				res.body.points.coreScoring.wicketTaken.batsmanBowler.should.be.exactly(15);
+				res.body.points.coreScoring.wicketTaken.batsmanNotBowler.should.be.exactly(25);
+				res.body.points.trumpMultipier.should.be.exactly(2);
+				res.body.points.updateSchedule.should.be.exactly('day');
+				res.body.teamFormat.should.have.length(7);
+				res.body.transfers.betweenPhases.should.be.exactly('unlimited');
+				res.body.transfers.phase1.should.be.exactly(120);
+				res.body.transfers.phase2.should.be.exactly(10);
+
+				done();
+			});
+	});
+});
