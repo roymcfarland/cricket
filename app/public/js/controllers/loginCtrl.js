2823d32f2d681ec130cdb66e7e0661e52f8d9eb0
Brooks Patton <brooks.patton@latitude40.com>
2015-02-12 16:44:04 -0700

    Added in console.logs to let us know when the user is logged in using parse promises.

--- app/public/js/controllers/loginCtrl.js
+++ app/public/js/controllers/loginCtrl.js
@@ -5,7 +5,14 @@ loginCtrl.controller("loginController", function() {
 	var vm = this;
 	vm.heading = "Please login by using the form below!";
 	vm.doLogin = function() {
-		Parse.User.logIn(vm.loginData.username, vm.loginData.password)
+		var userPromise = Parse.User.logIn(vm.loginData.username, vm.loginData.password)
+			.then(function(user){
+				console.log('***logging user in***');
+				console.log(typeof user);
+			}, function(error){
+				console.log('***error logging user in');
+				console.log(error);
+			});
 			/*
 			if (Parse.User.current()) {
 				alert("Welcome back! You are now logged in!")
