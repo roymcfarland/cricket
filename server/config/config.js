18d2003768f52a3dc3d31a89234b908f3168cb6c
Brooks Patton <brooks.patton@latitude40.com>
2015-02-17 11:28:08 -0700

    Created cricket duel configuration file and put the parse rest api keys in it.

---
+++ server/config/config.js
@@ -0,0 +1,8 @@
+var cricketDuelConfig = {
+	parse: {
+		applicationId: 'GeuNrmGKg5XYigjeBfB9w9mQWqp4WFWHDYqQPIzD',
+		masterKey: 'oGHICAf2xBZIGcIpehCUoHgUrvFBGUAPLgOAp9Dc'
+	}
+};
+
+module.exports = cricketDuelConfig;
