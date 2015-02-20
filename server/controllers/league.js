ea78e84291430c4446790cafa0c3c3261e27f125
Brooks Patton <brooks.patton@latitude40.com>
2015-02-20 11:23:18 -0700

    Returns a 515 error when no name is provided when creating a new league.

--- server/controllers/league.js
+++ server/controllers/league.js
@@ -1,10 +1,16 @@
 var superagent = require('superagent');
 var config = require('../config/config');
+var baseRules = require('../config/rules.json');
+
+var setRules = function(ruleChanges){
+	if(!ruleChanges) return baseRules;
+};
 
 var LeagueController = function(){};
 
 LeagueController.prototype.create = function(req, res) {
 	if('objectId' in req.body == false) return res.sendStatus(404);
+	if('name' in req.body == false) return res.sendStatus(515);
 
 	superagent
 		.get('https://api.parse.com/1/users/' + req.params.objectId)
@@ -12,6 +18,25 @@ LeagueController.prototype.create = function(req, res) {
 		.set('X-Parse-Master-Key', config.parse.masterKey)
 		.end(function(response){
 			if(response.body.error) return res.sendStatus(404);
+
+			var rules = setRules(req.params.rules || null);
+
+			superagent
+				.post('https://api.parse.com/1/classes/League')
+				.set('X-Parse-Application-Id', config.parse.applicationId)
+				.set('X-Parse-Master-Key', config.parse.masterKey)
+				.set('Content-Type', 'application/json')
+				.send({
+					"totalScore": 0,
+					"owner": req.params.objectId,
+					"rules": rules,
+					"name": req.params.name
+				})
+				.end(function(createLeagueResponse){
+					if(createLeagueResponse.body.error) return res.status(516).send(createLeagueResponse.body.error);
+
+					res.sendStatus(200);
+				});
 		});
 };
 
--- server/test/api-creating-new-league-test.js
+++ server/test/api-creating-new-league-test.js
@@ -1,7 +1,45 @@
 var supertest = require('supertest');
+var config = require('../config/config');
+
+var testUser;
 
 var requestLocal = supertest('http://localhost:3000');
 var requestParse = supertest('https://api.parse.com');
+
+describe('Creating the test user', function(){
+	it('creating new user on Parse', function(done) {
+		requestParse
+			.post('/1/users')
+			.set('Content-Type', 'application/json')
+			.set('X-Parse-Application-Id', config.parse.applicationId)
+			.set('X-Parse-Master-Key', config.parse.masterKey)
+			.send({
+				"username": "test",
+				"password": "password",
+				"email": "brooks.patton@latitude40.com",
+				"newUser": true
+			})
+			.expect(201)
+			.end(function(err, res) {
+				if(err) return done(err);
+
+				res.body.createdAt.should.have.type('string');
+				res.body.objectId.should.have.type('string');
+				res.body.sessionToken.should.have.type('string');
+				testUser = res.body.objectId;
+				done();
+			});
+	});
+
+	it('Should give the user defaults', function(done) {
+		request = supertest('http://localhost:3000');
+
+		request
+			.post('/api/users/' + testUser)
+			.expect(200)
+			.end(done);
+	});
+});
 
 describe('Creating a new league', function(){
 	it('should return a 404 when the user id is not provided', function(done){
@@ -15,9 +53,33 @@ describe('Creating a new league', function(){
 		requestLocal
 			.post('/api/league')
 			.send({
-				objectId: 'notreal'
+				objectId: 'notreal',
+				name: 'not a real name'
 			})
 			.expect(404)
 			.end(done);
+	});
+
+	it('should return a 515 error when no name is sent', function(done){
+		requestLocal
+			.post('/api/league')
+			.send({
+				objectId: 'notreal'
+			})
+			.expect(515)
+			.end(done);
+	});
+});
+
+describe('Cleaning up', function(){
+	it('by removing the test user', function(done) {
+		request = supertest('https://api.parse.com');
+
+		request
+			.del('/1/users/' + testUser)
+			.set('X-Parse-Application-Id', config.parse.applicationId)
+			.set('X-Parse-Master-Key', config.parse.masterKey)
+			.expect(200)
+			.end(done)
 	});
 });
