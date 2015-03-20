eedddd2c0a12e1df6ab17f9a2032251a6a2fe309
Brooks Patton <brooks.patton@latitude40.com>
2015-03-20 10:01:47 -0600

    Plan for a route to create a new user.

---
+++ server/test/bte/user/create.js
@@ -0,0 +1,7 @@
+var supertest = require('supertest');
+var should = require('should');
+
+var requestLocal = supertest('http://localhost:3000');
+var requestParse = supertest('https://api.parse.com');
+
+describe('--------------------User BTE Tests----------------------', function(){});
