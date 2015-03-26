ccf6ac59dcee7d3592e5d203bfdffb67c9dcc37c
Brooks Patton <brooks.patton@latitude40.com>
2015-03-26 11:34:32 -0600

    Started creating authentication tests.

---
+++ server/test/authentication-tests.js
@@ -0,0 +1,6 @@
+var supertest = require('supertest');
+var should = require('should');
+
+var requestLocal = supertest('http://localhost:3000');
+var requestParse = supertest('https://api.parse.com');
+
