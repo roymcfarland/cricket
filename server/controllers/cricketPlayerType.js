da3c4a583f8a2eb094d48804206dc8029ab64cf6
Brooks Patton <brooks.patton@latitude40.com>
2015-04-03 17:23:57 -0600

    The name is required.

--- server/controllers/cricketPlayerType.js
+++ server/controllers/cricketPlayerType.js
@@ -1,9 +1,18 @@
+var Validatorjs = require('validatorjs');
+
 var CricketPlayerTypeController = function(){};
 
 CricketPlayerTypeController.prototype.create = function(req, res) {
 	var admin = req.user.admin;
 
 	if(!admin) return res.sendStatus(403);
+
+	var validationRules = {
+		name: 'required'
+	};
+	var validation = new Validatorjs(req.body, validationRules);
+
+	if(validation.fails()) return res.status(428).send({errors: validation.errors.all()});
 };
 
 module.exports = CricketPlayerTypeController;
--- server/test/cricketPlayerTypes-tests.js
+++ server/test/cricketPlayerTypes-tests.js
@@ -1,7 +1,10 @@
 var supertest = require('supertest');
+var config = require('../config/config');
 
 var requestLocal = supertest('http://localhost:3000');
+var requestParse = supertest('https://api.parse.com');
 var testUser;
+var testAdmin;
 
 describe('Preparing for the Cricket Player Type tests by creating a', function(){
 	it('testUser.', function(done){
@@ -21,6 +24,28 @@ describe('Preparing for the Cricket Player Type tests by creating a', function()
 				done();
 			});
 	});
+	it('a testAdmin.', function(done){
+		requestParse
+			.post('/1/users')
+			.set('X-Parse-Application-Id', config.parse.applicationId)
+			.set('X-Parse-REST-API-Key', config.parse.apiKey)
+			.send({
+				username: 'testAdmin',
+				password: 'password',
+				email: 'testAdmin@latitude40.com',
+				totalScore: 0,
+				Money: 0,
+				admin: true
+			})
+			.expect(201)
+			.end(function(err, res){
+				if(err) return done(err);
+				if(res.body.code) return done(res.body);
+
+				testAdmin = res.body;
+				done();
+			});
+	});
 });
 
 describe('Sending a POST to /api/v1/cricketPlayerTypes', function(){
@@ -36,6 +61,22 @@ describe('Sending a POST to /api/v1/cricketPlayerTypes', function(){
 				.expect(403)
 				.end(done);
 		});
+		it('when the name is not passed in.', function(done){
+			requestLocal
+				.post('/api/v1/cricketPlayerTypes')
+				.send({
+					sessionToken: testAdmin.sessionToken,
+					// name: 'testCricketPlayerType',
+					lineUpMinimum: 2
+				})
+				.expect(428)
+				.end(function(err, res){
+					if(err) return done(err);
+
+					res.body.errors.name[0].should.be.exactly('The name field is required.');
+					done();
+				});
+		});
 	});
 });
 
@@ -45,6 +86,15 @@ describe('Cleaning up after the Cricket Player Type tests by deleting the', func
 			.del('/api/v1/users/' + testUser.objectId)
 			.send({
 				sessionToken: testUser.sessionToken
+			})
+			.expect(200)
+			.end(done);
+	});
+	it('testAdmin.', function(done){
+		requestLocal
+			.del('/api/v1/users/' + testAdmin.objectId)
+			.send({
+				sessionToken: testAdmin.sessionToken
 			})
 			.expect(200)
 			.end(done);
