ba2a36f3acff1b8e9f5c16b757c50b2455ecbae2
Brooks Patton <brooks.patton@latitude40.com>
2015-04-03 11:07:41 -0600

    The input data must be correct or it will not pass server side validation.

--- server/controllers/cricketPlayer.js
+++ server/controllers/cricketPlayer.js
@@ -1,9 +1,19 @@
+var Validatorjs = require('validatorjs');
+
 var CricketPlayerController = function(){};
 
 CricketPlayerController.prototype.create = function(req, res) {
 	var admin = req.user.admin;
+	var validationRules = {
+		name: 'required|alpha_num',
+		team: 'required|alpha_num',
+		cost: 'required|numeric',
+		cricketPlayerTypeId: 'required|alpha_num'
+	};
+	var validation = new Validatorjs(req.body, validationRules);
 
-	if(!admin) return res.sendStatus(403); 
+	if(!admin) return res.sendStatus(403);
+	if(validation.fails()) return res.status(428).send({errors: validation.errors.all()});
 };
 
 module.exports = CricketPlayerController;
--- server/test/cricketPlayers-tests.js
+++ server/test/cricketPlayers-tests.js
@@ -7,6 +7,7 @@ var requestLocal = supertest('http://localhost:3000');
 
 var testCricketPlayerType;
 var testUser;
+var testAdmin;
 
 describe('Preparing for the Cricket Player tests by creating', function(){
 	it('a Cricket Player Type.', function(done){
@@ -44,6 +45,28 @@ describe('Preparing for the Cricket Player tests by creating', function(){
 				done();
 			});
 	});
+	it('a testAdmin.', function(done){
+		requestParse
+			.post('/1/users')
+			.set('X-Parse-Application-Id', config.parse.applicationId)
+			.set('X-Parse-REST-API-Key', config.parse.apiKey)
+			.send({
+				username: 'testAdmin',
+				password: 'password',
+				email: 'testAdmin@latitude40.com',
+				totalScore: 0,
+				Money: 0,
+				admin: true
+			})
+			.expect(201)
+			.end(function(err, res){
+				if(err) return done(err);
+				if(res.body.code) return done(res.body);
+
+				testAdmin = res.body;
+				done();
+			});
+	});
 });
 
 describe('Sending a POST to /api/v1/cricketPlayers', function(){
@@ -56,6 +79,154 @@ describe('Sending a POST to /api/v1/cricketPlayers', function(){
 				})
 				.expect(403)
 				.end(done);
+		});
+		it('when the name is not included.', function(done){
+			requestLocal
+				.post('/api/v1/cricketPlayers')
+				.send({
+					sessionToken: testAdmin.sessionToken,
+					// name: 'testCricketPlayer',
+					team: 'testCricketPlayerTeam',
+					cost: 9000,
+					cricketPlayerTypeId: testCricketPlayerType.objectId
+				})
+				.expect(428)
+				.end(function(err, res){
+					if(err) return done(err);
+
+					res.body.errors.name[0].should.be.exactly('The name field is required.');
+					done();
+				});
+		});
+		it('when the team is not included.', function(done){
+			requestLocal
+				.post('/api/v1/cricketPlayers')
+				.send({
+					sessionToken: testAdmin.sessionToken,
+					name: 'testCricketPlayer',
+					// team: 'testCricketPlayerTeam',
+					cost: 9000,
+					cricketPlayerTypeId: testCricketPlayerType.objectId
+				})
+				.expect(428)
+				.end(function(err, res){
+					if(err) return done(err);
+
+					res.body.errors.team[0].should.be.exactly('The team field is required.');
+					done();
+				});
+		});
+		it('when the cost is not included.', function(done){
+			requestLocal
+				.post('/api/v1/cricketPlayers')
+				.send({
+					sessionToken: testAdmin.sessionToken,
+					name: 'testCricketPlayer',
+					team: 'testCricketPlayerTeam',
+					// cost: 9000,
+					cricketPlayerTypeId: testCricketPlayerType.objectId
+				})
+				.expect(428)
+				.end(function(err, res){
+					if(err) return done(err);
+
+					res.body.errors.cost[0].should.be.exactly('The cost field is required.');
+					done();
+				});
+		});
+		it('when the cricketPlayerTypeId is not included.', function(done){
+			requestLocal
+				.post('/api/v1/cricketPlayers')
+				.send({
+					sessionToken: testAdmin.sessionToken,
+					name: 'testCricketPlayer',
+					team: 'testCricketPlayerTeam',
+					cost: 9000,
+					// cricketPlayerTypeId: testCricketPlayerType.objectId
+				})
+				.expect(428)
+				.end(function(err, res){
+					if(err) return done(err);
+
+					res.body.errors.cricketPlayerTypeId[0].should.be.exactly('The cricketPlayerTypeId field is required.');
+					done();
+				});
+		});
+		it('when the name is not alphanumeric.', function(done){
+			requestLocal
+				.post('/api/v1/cricketPlayers')
+				.send({
+					sessionToken: testAdmin.sessionToken,
+					name: 'ry876092&^%T',
+					// name: 'testCricketPlayer',
+					team: 'testCricketPlayerTeam',
+					cost: 9000,
+					cricketPlayerTypeId: testCricketPlayerType.objectId
+				})
+				.expect(428)
+				.end(function(err, res){
+					if(err) return done(err);
+
+					res.body.errors.name[0].should.be.exactly('The name field must be alphanumeric.');
+					done();
+				});
+		});
+		it('when the team is not alphanumeric.', function(done){
+			requestLocal
+				.post('/api/v1/cricketPlayers')
+				.send({
+					sessionToken: testAdmin.sessionToken,
+					name: 'testCricketPlayer',
+					team: 'argLJGF%$',
+					// team: 'testCricketPlayerTeam',
+					cost: 9000,
+					cricketPlayerTypeId: testCricketPlayerType.objectId
+				})
+				.expect(428)
+				.end(function(err, res){
+					if(err) return done(err);
+
+					res.body.errors.team[0].should.be.exactly('The team field must be alphanumeric.');
+					done();
+				});
+		});
+		it('when the cost is not numeric.', function(done){
+			requestLocal
+				.post('/api/v1/cricketPlayers')
+				.send({
+					sessionToken: testAdmin.sessionToken,
+					name: 'testCricketPlayer',
+					team: 'testCricketPlayerTeam',
+					cost: 'ayurs7t',
+					// cost: 9000,
+					cricketPlayerTypeId: testCricketPlayerType.objectId
+				})
+				.expect(428)
+				.end(function(err, res){
+					if(err) return done(err);
+
+					res.body.errors.cost[0].should.be.exactly('The cost must be a number.');
+					done();
+				});
+		});
+		it('when the cricketPlayerTypeId is not alphanumeric.', function(done){
+			requestLocal
+				.post('/api/v1/cricketPlayers')
+				.send({
+					sessionToken: testAdmin.sessionToken,
+					name: 'testCricketPlayer',
+					team: 'testCricketPlayerTeam',
+					cost: 9000,
+					cricketPlayerTypeId: 'aeisrtluj654#%$'
+					// cricketPlayerTypeId: testCricketPlayerType.objectId
+				})
+				.expect(428)
+				.end(function(err, res){
+					if(err) return done(err);
+
+					res.body.errors.cricketPlayerTypeId[0].should.be.exactly('The cricketPlayerTypeId field must be alphanumeric.');
+					done();
+				});
 		});
 	});
 });
@@ -75,12 +246,21 @@ describe('Cleaning up after the Cricket Player tests by deleting', function(){
 			});
 	});
 	it('the testUser.', function(done){
-			requestLocal
-				.del('/api/v1/users/' + testUser.objectId)
-				.send({
-					sessionToken: testUser.sessionToken
-				})
-				.expect(200)
-				.end(done);
-		});
+		requestLocal
+			.del('/api/v1/users/' + testUser.objectId)
+			.send({
+				sessionToken: testUser.sessionToken
+			})
+			.expect(200)
+			.end(done);
+	});
+	it('the testAdmin.', function(done){
+		requestLocal
+			.del('/api/v1/users/' + testAdmin.objectId)
+			.send({
+				sessionToken: testAdmin.sessionToken
+			})
+			.expect(200)
+			.end(done);
+	});
 });
