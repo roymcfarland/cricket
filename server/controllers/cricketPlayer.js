c3f3d78bffff41c6421d7d89b2c8def81c7ca81c
Brooks Patton <brooks.patton@latitude40.com>
2015-04-03 15:34:50 -0600

    The name must be alphanumeric.

--- server/controllers/cricketPlayer.js
+++ server/controllers/cricketPlayer.js
@@ -74,8 +74,13 @@ CricketPlayerController.prototype.getOne = function(req, res) {
 
 CricketPlayerController.prototype.update = function(req, res) {
 	var admin = req.user.admin;
+	var validationRules = {
+		name: 'alpha_num'
+	};
+	var validation = new Validatorjs(req.body, validationRules);
 
 	if(!admin) return res.sendStatus(403);
+	if(validation.fails()) return res.status(428).send({errors: validation.errors.all()});
 };
 
 module.exports = CricketPlayerController;
--- server/test/cricketPlayers-tests.js
+++ server/test/cricketPlayers-tests.js
@@ -327,6 +327,25 @@ describe('Sending a PUT to /api/v1/cricketPlayers/:objectId', function(){
 				.expect(403)
 				.end(done);
 		});
+		it('when the name is not alphanumeric.', function(done){
+			requestLocal
+				.put('/api/v1/cricketPlayers/' + testCricketPlayer.objectId)
+				.send({
+					sessionToken: testAdmin.sessionToken,
+					name: 'aiestnrh87$$%',
+					// name: 'updatedTestCricketPlayer',
+					team: 'updatedTestCricketPlayerTeam',
+					cost: 8,
+					cricketPlayerTypeId: testCricketPlayerType.objectId
+				})
+				.expect(428)
+				.end(function(err, res){
+					if(err) return done(err);
+
+					res.body.errors.name[0].should.be.exactly('The name field must be alphanumeric.');
+					done();
+				});
+		});
 	});
 });
 
